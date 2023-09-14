bb49255a912619f0003ab58ff0bd742a4009c785
quan11 <quanpdhe170415@fpt.edu.vn>
2023-09-14 16:43:26 +0700

    Add module app
    Add module app

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/AppComponent/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
