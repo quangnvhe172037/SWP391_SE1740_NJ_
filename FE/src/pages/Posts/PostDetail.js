1f4973c4a0f3268c2bd1c663f22a3d1ebbed8d96
annthe170622 <annthe170622@fpt.edu.vn>
2023-09-27 22:57:07 +0700

    update Post Detail css

--- FE/src/pages/Posts/PostDetail.js
+++ FE/src/pages/Posts/PostDetail.js
@@ -1,18 +1,19 @@
-// Tệp PostDetail.js
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './style.css';
+import { format } from "date-fns";
 
+const API_URL = "http://localhost:8080/";
+const Local_URL = "http://localhost:8081/";
 
-const API_URL = "http://localhost:8080/posts";
 const PostDetail = () => {
     const { postId } = useParams();
     const [post, setPost] = useState({});
 
     useEffect(() => {
         // Truy vấn dữ liệu của bài viết từ API sử dụng postId
-        axios.get(`${API_URL}/view/${postId}`)
+        axios.get(`${API_URL}posts/view/${postId}`)
             .then((response) => {
                 setPost(response.data);
             })
@@ -20,15 +21,46 @@ const PostDetail = () => {
                 console.error('Error fetching post data: ', error);
             });
     }, [postId]);
-    console.log(post);
-    return (
 
-        <div>
-            <h1>{post.title}</h1>
-            <img src={post.image} alt={post.title} />
-            <p>{post.postData}</p>
-            {/* Hiển thị các thông tin khác của bài viết */}
+    function formatTextToParagraphs(text) {
+        if (text && text.trim() !== '') {
+            const paragraphs = text.split('/n').filter((paragraph) => paragraph.trim() !== '');
+            return paragraphs.map((paragraph, index) => <p key={index}>{paragraph}</p>);
+        } else {
+            return null; // Hoặc hiển thị thông báo khi không có dữ liệu.
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 mx-auto">
+                    <hr className="tm-hr-primary tm-mb-30" />
+                    <div className="effect-lily tm-post-link tm-pt-40">
+                        <div className="tm-post-link-inner d-flex justify-content-center align-items-center">
+                            {post.image && (
+                                <img
+                                    src={`${Local_URL}${post.image}`}
+                                    alt={post.title}
+                                    className="img-fluid custom-image"
+                                />
+                            )}
+                        </div>
+                    </div>
+                    <div className="blog-entry mt-4">
+                        {post.title && <h2 className="post-title">{post.title}</h2>}
+                        {post.dateCreate && (
+                            <div className="date">
+                                {format(new Date(post.dateCreate), 'dd-MM-yyyy')}
+                            </div>
+                        )}
+                        {formatTextToParagraphs(post.postData)}
+                        <p>Author: {post.user && post.user.lastName + ' ' + post.user.firstName}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
 export default PostDetail;
