970e87c8fe96fd8054ca8dfa0829ab1d863a0584
anbghe170027 <anbghe170027@fpt.edu.vn>
2023-11-07 18:20:37 +0700

    Update for SubjectCategoriesRepo

--- BE/src/main/java/com/example/onlinequiz/Repo/SubjectCategoriesRepository.java
+++ BE/src/main/java/com/example/onlinequiz/Repo/SubjectCategoriesRepository.java
@@ -2,6 +2,6 @@ package com.example.onlinequiz.Repo;
 
 import com.example.onlinequiz.Model.SubjectCategories;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// Day la chu thich cho SubjectCategoriesRepo
 public interface SubjectCategoriesRepository extends JpaRepository<SubjectCategories, Integer> {
 }
